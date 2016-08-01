58f807c971eea7e79e4ac09941edcce842480dae
bhrgv <bhargav2014duggu@gmail.com>
2016-08-01 20:51:55 +0530

    changes in urls.py in ista proj

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
-import views
+from Instagram import views
 
 from Instagram import settings
 
